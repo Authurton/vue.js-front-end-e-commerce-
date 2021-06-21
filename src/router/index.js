a034cd59107a7f235699f1e3b76ee69ef7afd39d
Authurton <Authurlunga@gmail.com>
2021-06-21 13:30:54 +0200

    First commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from '../store'
+
 import Home from '../views/Home.vue'
+import Product from '../views/Product.vue'
+import Category from '../views/Category.vue'
+import Search from '../views/Search.vue'
+import Cart from '../views/Cart.vue'
+import SignUp from '../views/SignUp.vue'
+import Login from '../views/Login.vue'
+import MyAccount from '../views/MyAccount.vue'
+import Checkout from '../views/Checkout.vue'
+import Success from '../views/Success.vue'
 
 const routes = [
   {
@@ -8,18 +19,80 @@ const routes = [
     component: Home
   },
   {
+    
     path: '/about',
     name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+
+  {
+    path: '/search',
+    name: 'Search',
+    component: Search
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    component: Cart
+  },
+  {
+    path: '/cart/success',
+    name: 'Success',
+    component: Success
+  },
+  {
+    path: '/cart/checkout',
+    name: 'Checkout',
+    component: Checkout,
+    meta: {
+      requireLogin: true
+    }
+  },
+  {
+    path: '/sign-up',
+    name: 'SignUp',
+    component: SignUp
+  },
+  {
+    path: '/log-in',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/my-account',
+    name: 'MyAccount',
+    component: MyAccount,
+    meta: {
+      requireLogin: true
+    }
+  },
+  
+  {
+    path: '/:category_slug/:product_slug',
+    name: 'Product',
+    component: Product
+  },
+  {
+    path: '/:category_slug',
+    name: 'Category',
+    component: Category
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+    next({ name: 'LogIn', query: { to: to.path } });
+  } else {
+    next()
+  }
 })
 
 export default router
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="hero is-medium is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">
+          Welcome to Jackets
+        </p>
+        <p class="subtitle">
+          The best jacket store
+        </p>
+      </div>
+    </section>
+    <div class="columns-is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">
+          Latest products
+        </h2>
+      </div>
+      <ProductBox
+      v-for="product in latestProducts" :key="product.id" :product="product"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+import axios from 'axios'
+import ProductBox from '@/components/ProductBox'
 export default {
   name: 'Home',
+  data(){
+    return{
+      latestProducts:[]
+    }
+  },
   components: {
-    HelloWorld
+    ProductBox
+    
+  }, 
+  mounted(){
+    this.getLatestProducts()
+    document.title = 'Home  |Djackets'
+  },
+  methods:{
+    getLatestProducts(){
+      axios
+      .get('api/v1/latest-products/')
+      .then(response => {
+        this.latestProducts = response.data
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    }
   }
 }
 </script>
+
